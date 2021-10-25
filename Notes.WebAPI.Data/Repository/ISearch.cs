83bfa3cb5637de440b8f94c0e5f4abfb0dc6fe8a
Prasanna Kumar <pkumarstek@outlook.com>
2021-10-25 20:36:12 +0530

    first commit

---
+++ Notes.WebAPI.Data/Repository/ISearch.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Notes.WebAPI.Data.Models;
+
+namespace Notes.WebAPI.Data.Repository
+{
+    public interface ISearch<TEntity>
+    {
+        IEnumerable<TEntity> Search(string term);
+    }
+}
